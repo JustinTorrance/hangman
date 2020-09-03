bef7f017d0aa932c39363bbe9f7dea71b6dcba28
Justin Torrance <justin.torrance91@gmail.com>
2020-09-03 13:19:41 -0400

    Create components and set up static app

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    //returns a fragment instead of a div
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+      
+    </>
+  )
+}
+
+export default Header
